1dc82e6c3f9895f83cdfcdd10f7cf1f182c1432b
Redkhouya21 <redouankhouya188@gmail.com>
2025-01-06 15:40:34 +0100

    Add files via upload

---
+++ Projet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+typedef struct {
+    int IdClient;
+    char nom[20];
+    char prenom[20];
+    char adresse[120];
+    int tele;
+    char email[120];
+};
+typedef struct {
+    int Id_voyage;
+    int duree_voyage;
+    char destination[20];
+    
+}
